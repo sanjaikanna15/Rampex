2c14cbf618fb31345ccab054e0daefe4e2d8adb8
sanjaikanna15 <sanjaikanna42@gmail.com>
2025-05-16 14:10:56 +0530

    Day1

---
+++ day1.c/namagesal.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+void main() {
+    char name[],age[],salary[];
+    printf("Enter your name: ");
+    scanf("%c",&name);
+    printf("Enter your age: "); 
+    scanf("%c",&age);
+    printf("Enter your salary: ");
+    scanf("%c",&salary);
+    printf("name: %c\n",name);
+    printf("age: %c\n",age);
+    printf("salary: %c\n",salary);
+}
